e4873f7b3aac625b039eb600480a87e6537c0b64
Nazma10 <nazmanazar5@gmail.com>
2023-07-20 23:22:23 +0530

    Updates

--- lib/UIs/themes/colors.dart
+++ lib/UIs/themes/colors.dart
@@ -1,6 +1,6 @@
 // import 'package:flutter/material.dart' show Color;
 
-// class ColorsFrave {
+// class Colors {
 
 //   static const Color primary = Color(0xff0C1C33);
 //   static const Color secundary = Color(0xff2FACBB);
--- lib/UIs/themes/colors_frave.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:flutter/material.dart' show Color;
-
-// class ColorsFrave {
-
-//   static const Color primary = Color(0xff0C1C33);
-//   static const Color secundary = Color(0xff2FACBB);
-//   static const Color background = Color(0xffFFFFFF);
-
-// }
