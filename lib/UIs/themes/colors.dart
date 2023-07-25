3359d176117533b90d4ff43c49a09b3c8c29d516
Luqman Fazal <2020is047@stu.ucsc.cmb.ac.lk>
2023-07-25 15:05:46 +0530

    Resolve imports and testing widgets

--- lib/UIs/themes/colors.dart
+++ lib/UIs/themes/colors.dart
@@ -1,9 +1,9 @@
-// import 'package:flutter/material.dart' show Color;
+import 'package:flutter/material.dart' show Color;
 
-// class ColorsTravelMate {
+class ColorsTravelMate {
 
-//   static const Color primary = Color(0xff0C1C33);
-//   static const Color secundary = Color(0xff2FACBB);
-//   static const Color tertiary = Color(0xffFFFFFF);
+  static const Color primary = Color(0xff0C1C33);
+  static const Color secundary = Color(0xff2FACBB);
+  static const Color tertiary = Color(0xffFFFFFF);
 
-// }
+}
