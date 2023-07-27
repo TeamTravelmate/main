e9a960cff65955b2019599e5aacb8f0fcbef5165
Hasantha Kariyawasam <2020cs090@stu.ucsc.cmb.ac.lk>
2023-07-27 13:20:51 +0530

    side nav uc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:main/UIs/widgets/side_menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,52 +25,53 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('TravelMate'),
-        ),
-        body: _tabs[_currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                color: Colors.blue,
-              ),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.search,
-                color: Colors.blue,
-              ),
-              label: 'Search',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.directions, color: Colors.blue),
-              label: 'Trip Planning',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.store, color: Colors.blue),
-              label: 'Marketplace',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.chat,
-                color: Colors.blue,
-              ),
-              label: 'Chat',
-            ),
-          ],
-        ),
-      ),
+    return const MaterialApp(
+        home: SideMenu(),
+      // Scaffold(
+      //   appBar: AppBar(
+      //     title: const Text('TravelMate'),
+      //   ),
+      //   body: _tabs[_currentIndex],
+      //   bottomNavigationBar: BottomNavigationBar(
+      //     currentIndex: _currentIndex,
+      //     onTap: (index) {
+      //       setState(() {
+      //         _currentIndex = index;
+      //       });
+      //     },
+      //     items: const [
+      //       BottomNavigationBarItem(
+      //         icon: Icon(
+      //           Icons.home,
+      //           color: Colors.blue,
+      //         ),
+      //         label: 'Home',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(
+      //           Icons.search,
+      //           color: Colors.blue,
+      //         ),
+      //         label: 'Search',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.directions, color: Colors.blue),
+      //         label: 'Trip Planning',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.store, color: Colors.blue),
+      //         label: 'Marketplace',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(
+      //           Icons.chat,
+      //           color: Colors.blue,
+      //         ),
+      //         label: 'Chat',
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
