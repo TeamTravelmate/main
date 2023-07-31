41dbdd34c0e441e6597f70e8b80256a62497587f
Hasantha Kariyawasam <2020cs090@stu.ucsc.cmb.ac.lk>
2023-07-31 18:15:39 +0530

    Welcome screen completed

--- lib/UIs/screens/Welcome/welcome.dart
+++ lib/UIs/screens/Welcome/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:main/UIs/themes/colors.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
@@ -9,12 +10,94 @@ class Welcome extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
+            // cover image
             Image.asset(
-              "assets/img/logo-travelmate.png",
+              "assets/img/cover.jpg",
+              height: MediaQuery.of(context).size.height * 0.55,
               width: MediaQuery.of(context).size.width,
               fit: BoxFit.cover,
             ),
-            const SizedBox(height: 46.1),
+
+            // logo
+            Image.asset(
+              "assets/img/logo-travelmate.png",
+              width: 300,
+              fit: BoxFit.cover,
+            ),
+
+            const SizedBox(height: 15),
+            // login
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 40.0,
+                vertical: 10,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: MaterialButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      color: ColorsTravelMate.primary,
+                      onPressed: () {},
+                      child: const Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // register
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 40.0,
+                vertical: 10,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: MaterialButton(
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(color: ColorsTravelMate.primary),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      onPressed: () {},
+                      child: const Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text(
+                          "Register",
+                          style: TextStyle(
+                              color: ColorsTravelMate.primary, fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Spacer(),
+            Spacer(),      
+
+            GestureDetector(
+              onTap: () {},
+              child: const Text(
+                "Continue as guest",
+                style: TextStyle(
+                  color: ColorsTravelMate.secundary,
+                  fontWeight: FontWeight.w500,
+                  
+                ),
+              ),
+            ),
+
+            Spacer(),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:main/UIs/screens/Registration/NameInputScreen.dart';
+import 'package:main/UIs/screens/Welcome/welcome.dart';
 import 'package:main/UIs/screens/Welcome/welcome_screen_2.dart';
 import 'package:main/UIs/screens/Welcome/welcome_screen_3.dart';
 import 'UIs/screens/Welcome/welcome_screen_1.dart';
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blueGrey,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: const WelcomeScreenOne(),
+      home: const Welcome(),
     );
   }
   
