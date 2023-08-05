b8bd2a49450b5fc2725b752a1ee7bcddf1d04e6b
Luqman Fazal <2020is047@stu.ucsc.cmb.ac.lk>
2023-08-05 19:26:09 +0530

    Added day view for the map

--- lib/UIs/screens/map/map_todo.dart
+++ lib/UIs/screens/map/map_todo.dart
@@ -1,8 +1,14 @@
+import 'package:main/UIs/themes/colors.dart';
 import 'package:main/UIs/widgets/text_custom.dart';
+import 'package:main/UIs/widgets/todo_day_tile.dart';
+import 'package:main/UIs/widgets/top_back_btn.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter/material.dart';
+import 'package:timelines/timelines.dart';
+
+import '../../widgets/tripCard_widget.dart';
 
 class MapToDoScreen extends StatelessWidget {
   const MapToDoScreen({super.key});
@@ -10,10 +16,8 @@ class MapToDoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: const Text("Map")),
-      ),
       body: SlidingUpPanel(
+        margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
         body: FlutterMap(
           options: MapOptions(
             center: const LatLng(6.9020, 79.8611),
@@ -25,124 +29,90 @@ class MapToDoScreen extends StatelessWidget {
             TileLayer(
               urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
               userAgentPackageName: "flutter_map",
-            )
+            ),
+            //floating back button in the top left corner
+            const TopBackBtn(),
           ],
         ),
-        header: Container(
-          width: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    TextCustom(
-                      text: "Trip To Galle",
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      fontSize: MediaQuery.of(context).size.width / 12,
+        panelBuilder: (sc) => MapSlidingPanel(sc: sc, context: context),
+        collapsed: Container(
+          decoration: const BoxDecoration(
+            color: ColorsTravelMate.tertiary,
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+          child: const tripCard.map(
+            tripLocationTitle: "Colombo",
+            location: "Sri Lanka",
+            tripDuration: "3 days",
+            tripmates: "3",
+          ),
+        ),
+        minHeight: MediaQuery.of(context).size.height / 6,
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+      ),
+    );
+  }
+}
+
+class MapSlidingPanel extends StatelessWidget {
+  const MapSlidingPanel({
+    super.key,
+    required this.sc,
+    required this.context,
+  });
+
+  final ScrollController sc;
+  final BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.only(top:8.0),
+        child: Timeline.tileBuilder(
+          controller: sc,
+          theme: TimelineThemeData(
+            color: ColorsTravelMate.primary,
+            nodePosition: 0.2,
+            connectorTheme: const ConnectorThemeData(
+              thickness: 2.5,
+            ),
+          ),
+          builder: TimelineTileBuilder.connected(
+            connectionDirection: ConnectionDirection.after,
+            itemCount: 3,
+            indicatorBuilder: (_, index) => const DotIndicator(
+              size: 20,
+              color: ColorsTravelMate.primary,
+            ),
+            connectorBuilder: (_, index, ___) => const SolidLineConnector(
+              color: ColorsTravelMate.secundary,
+            ),
+            oppositeContentsBuilder: (_, index) => Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextCustom(
+                text: "Day ${index + 1}",
+                color: ColorsTravelMate.primary,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            contentsBuilder: (_, index) => SizedBox(
+              width: MediaQuery.of(context).size.width / 1.25,
+              child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    child: const TodoDayTile(
+                      locations: "Galle Face Green, Lotus tower, Floating market",
+                      approxBudget: "Rs. 5000",
+                      weather: "Sunny",
                     ),
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.edit,
-                            size: MediaQuery.of(context).size.width / 20)),
-                  ],
-                ),
-                //icon for a heart
-                Row(
-                  //align the icons to the right
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.favorite_border,
-                          size: MediaQuery.of(context).size.width / 20,
-                          color: Colors.red,
-                        )),
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.ios_share_rounded,
-                            size: MediaQuery.of(context).size.width / 20)),
-                  ],
-                ),
-              ],
+                    onTap: () {
+                      print("Day ${index + 1} tapped");
+                    },
+                  )),
             ),
           ),
         ),
-        panel: Center(child: Text("ToDo List")),
-        collapsed: Flex(
-          direction: Axis.horizontal,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Row(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width / 50),
-                            child: Icon(
-                              Icons.location_on_outlined,
-                              size: MediaQuery.of(context).size.width / 25,
-                              color: Colors.black,
-                            ),
-                          ),
-                          TextCustom(
-                            text: "Galle Fort, Unawatuna Beach",
-                            color: Colors.black,
-                            fontSize: MediaQuery.of(context).size.width / 25,
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width / 50),
-                            child: Icon(
-                              Icons.calendar_today_outlined,
-                              size: MediaQuery.of(context).size.width / 25,
-                              color: Colors.black,
-                            ),
-                          ),
-                          TextCustom(
-                            text: "July 3, 2023  - July 6, 2023 (3 days)",
-                            color: Colors.black,
-                            fontSize: MediaQuery.of(context).size.width / 25,
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width / 50),
-                            child: Icon(
-                              Icons.account_circle_outlined,
-                              size: MediaQuery.of(context).size.width / 25,
-                              color: Colors.black,
-                            ),
-                          ),
-                          TextCustom(
-                            text: "3",
-                            color: Colors.black,
-                            fontSize: MediaQuery.of(context).size.width / 25,
-                          ),
-                        ],
-                      ),
-                    ]),
-              ),
-            ),
-            IconButton(
-                onPressed: () {}, icon: Icon(Icons.keyboard_arrow_up_outlined)),
-          ],
-        ),
-        minHeight: MediaQuery.of(context).size.height / 6,
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-      ),
-    );
+      );
   }
 }
