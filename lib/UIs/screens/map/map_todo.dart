1fa42812ee8f247fa9866484f47f24e75578c89c
Luqman Fazal <2020is047@stu.ucsc.cmb.ac.lk>
2023-08-11 18:35:23 +0530

    Added google map with style

--- lib/UIs/screens/map/map_todo.dart
+++ lib/UIs/screens/map/map_todo.dart
@@ -1,39 +1,26 @@
 import 'package:main/UIs/themes/colors.dart';
-import 'package:main/UIs/widgets/text_custom.dart';
+import 'package:main/UIs/widgets/map_widget.dart';
 import 'package:main/UIs/widgets/todo_day_tile.dart';
-import 'package:main/UIs/widgets/top_back_btn.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:flutter/material.dart';
-import 'package:timelines/timelines.dart';
 
 import '../../widgets/tripCard_widget.dart';
 
-class MapToDoScreen extends StatelessWidget {
+class MapToDoScreen extends StatefulWidget {
   const MapToDoScreen({super.key});
 
+  @override
+  State<MapToDoScreen> createState() => _MapToDoScreenState();
+}
+
+class _MapToDoScreenState extends State<MapToDoScreen> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SlidingUpPanel(
         margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-        body: FlutterMap(
-          options: MapOptions(
-            center: const LatLng(6.9020, 79.8611),
-            zoom: 14.0,
-            minZoom: 10,
-            maxZoom: 18,
-          ),
-          children: [
-            TileLayer(
-              urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-              userAgentPackageName: "flutter_map",
-            ),
-            //floating back button in the top left corner
-            const TopBackBtn(),
-          ],
-        ),
+        body: CustomMap(),
         panelBuilder: (sc) => MapSlidingPanel(sc: sc, context: context),
         collapsed: Container(
           decoration: const BoxDecoration(
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:main/UIs/screens/Registration/NameInputScreen.dart';
-import 'package:main/UIs/screens/Welcome/welcome.dart';
-import 'package:main/UIs/screens/Welcome/welcome_screen_2.dart';
-import 'package:main/UIs/screens/Welcome/welcome_screen_3.dart';
-import 'package:main/UIs/screens/addCard/address.dart';
-import 'package:main/UIs/screens/leaderboard/leaderboard.dart';
-import 'package:main/UIs/screens/login/login_page.dart';
-import 'package:main/UIs/screens/myorders/orders_1.dart';
-import 'package:main/UIs/screens/emergency_support/emergency_support_home.dart';
-import 'package:main/UIs/screens/login/login_page.dart';
-import 'package:main/UIs/widgets/bottom_nav.dart';
-import 'package:main/UIs/screens/profile/profile.dart';
-import 'UIs/screens/Welcome/welcome_screen_1.dart';
+import 'UIs/screens/map/map_todo.dart';
 
 // Main function
 void main() {
@@ -36,9 +24,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: GoogleFonts.poppins().fontFamily,
         primarySwatch: Colors.blueGrey,
+        useMaterial3: true,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: const BottomNav(),
+      home: const MapToDoScreen(),
     );
   }
 }
